07fbbbc995abc8caa43936144519dcaee8875fdb
imran ansari <stasim101@gmail.com>
2021-02-06 10:01:26 +0530

    Project initialized

---
+++ chaipoint/src/main/java/com/dunzo/chaipoint/ChaipointApplication.java
@@ -0,0 +1,13 @@
+package com.dunzo.chaipoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaipointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaipointApplication.class, args);
+	}
+
+}
---
+++ chaipoint/src/test/java/com/dunzo/chaipoint/ChaipointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dunzo.chaipoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaipointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
